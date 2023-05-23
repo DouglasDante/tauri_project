907f0babe0c1c39734bdfd6dc28867ff14ed1224
DouglasDante <96195302+DouglasDante@users.noreply.github.com>
2023-05-24 00:00:21 +0900

    q

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,15 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("안녕하세요, {}! 러스트에서 작업하신 걸 환영합니다!", name)
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
